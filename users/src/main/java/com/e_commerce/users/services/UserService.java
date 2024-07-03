4cf98d4143520fc45c67da76480f9380e480ab70
João Lourenço <joaodslourenco@gmail.com>
2024-07-03 20:42:25 -0300

    feat: add user repository injection at service

--- users/src/main/java/com/e_commerce/users/services/UserService.java
+++ users/src/main/java/com/e_commerce/users/services/UserService.java
@@ -3,12 +3,14 @@ package com.e_commerce.users.services;
 import com.e_commerce.users.dtos.UserRecordCreateDto;
 import com.e_commerce.users.mappers.UserMapper;
 import com.e_commerce.users.models.UserModel;
+import com.e_commerce.users.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
     private final UserMapper userMapper;
 
 
