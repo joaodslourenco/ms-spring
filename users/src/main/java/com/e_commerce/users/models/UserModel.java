0d84509e8444ce03775132efcf8738dc0e6df69c
João Lourenço <joaodslourenco@gmail.com>
2024-07-09 17:49:53 -0300

    fix: add table name

--- users/src/main/java/com/e_commerce/users/models/UserModel.java
+++ users/src/main/java/com/e_commerce/users/models/UserModel.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tb_users")
 public class UserModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
