2da3626c748010e74c960e7b86ced40742c28649
João Lourenço <joaodslourenco@gmail.com>
2024-07-09 14:54:21 -0300

    feat: add swagger documentation

--- users/src/main/java/com/e_commerce/users/controllers/UserController.java
+++ users/src/main/java/com/e_commerce/users/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.e_commerce.users.controllers;
 import com.e_commerce.users.dtos.UserRecordCreateDto;
 import com.e_commerce.users.models.UserModel;
 import com.e_commerce.users.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,10 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
+    @Operation(summary = "Creates a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully")
+    })
     public ResponseEntity<UserModel> save(@RequestBody @Valid UserRecordCreateDto userRecordCreateDto) {
         return new ResponseEntity<>(userService.save(userRecordCreateDto), HttpStatus.CREATED);
     }
