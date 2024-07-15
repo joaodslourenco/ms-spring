dde66c1cf688c4cebb7fde1b94e9f93e78e98c5a
João Lourenço <joaodslourenco@gmail.com>
2024-07-15 20:28:16 -0300

    feat: adjustment to swagger documentation at 400 status code

--- users/src/main/java/com/e_commerce/users/controllers/UserController.java
+++ users/src/main/java/com/e_commerce/users/controllers/UserController.java
@@ -4,14 +4,20 @@ import com.e_commerce.users.annotations.HasRole;
 import com.e_commerce.users.dtos.UserRecordCreateDto;
 import com.e_commerce.users.dtos.UserRecordUpdateDto;
 import com.e_commerce.users.enums.ERole;
+import com.e_commerce.users.exceptions.details.BadRequestExceptionDetails;
 import com.e_commerce.users.models.UserModel;
 import com.e_commerce.users.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +26,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Tag(name = "User Controller")
 public class UserController {
     private final UserService userService;
 
@@ -27,7 +34,12 @@ public class UserController {
     @Operation(summary = "Creates a new user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "User created successfully"),
-            @ApiResponse(responseCode = "400", description = "User already exists")
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "User already exists",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BadRequestExceptionDetails.class)))
     })
     public ResponseEntity<UserModel> save(@RequestBody @Valid UserRecordCreateDto userRecordCreateDto) {
         return new ResponseEntity<>(userService.save(userRecordCreateDto), HttpStatus.CREATED);
@@ -38,9 +50,14 @@ public class UserController {
     @Operation(summary = "Finds an User by Id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Returns the requested user data"),
-            @ApiResponse(responseCode = "400", description = "The requested ID was not found")
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The requested ID was not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BadRequestExceptionDetails.class)))
     })
-    public ResponseEntity<UserModel> findById(@PathVariable UUID id) {
+    public ResponseEntity<UserModel> findById(@PathVariable @Parameter(description = "User ID to search for", required = true) UUID id) {
         return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
     }
 
@@ -49,7 +66,12 @@ public class UserController {
     @HasRole(ERole.ADMIN)
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User updated successfully"),
-            @ApiResponse(responseCode = "400", description = "The requested ID was not found")
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The requested ID was not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BadRequestExceptionDetails.class)))
     })
     public ResponseEntity<UserModel> update(@PathVariable UUID id, @RequestBody UserRecordUpdateDto userRecordUpdateDto) {
         return new ResponseEntity<>(userService.update(id, userRecordUpdateDto), HttpStatus.OK);
@@ -60,7 +82,12 @@ public class UserController {
     @Operation(summary = "Deletes an user")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User deleted successfully"),
-            @ApiResponse(responseCode = "400", description = "The requested ID was not found")
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The requested ID was not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BadRequestExceptionDetails.class)))
     })
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         userService.delete(id);
