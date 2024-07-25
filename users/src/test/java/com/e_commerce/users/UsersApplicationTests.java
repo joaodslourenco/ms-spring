42d6cecdbe31d57ecc0f48416a2c71cd35de2062
João Lourenço <joaodslourenco@gmail.com>
2024-07-25 09:15:18 -0300

    feat: add kafka, zookeeper and setup app to run in docker container

--- users/src/test/java/com/e_commerce/users/UsersApplicationTests.java
+++ users/src/test/java/com/e_commerce/users/UsersApplicationTests.java
@@ -2,8 +2,10 @@ package com.e_commerce.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsersApplicationTests {
 
 	@Test
