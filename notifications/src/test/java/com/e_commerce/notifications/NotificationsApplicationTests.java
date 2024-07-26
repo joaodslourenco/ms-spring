179fc5fe53a74b4ceedfc647c5ef10a50f026845
João Lourenço <joaodslourenco@gmail.com>
2024-07-26 10:08:02 -0300

    feat: add pending project files

---
+++ notifications/src/test/java/com/e_commerce/notifications/NotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
