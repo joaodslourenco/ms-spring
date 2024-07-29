01b16afef0dadbcb2d690f8d59b105b1c38e5ad4
João Lourenço <joaodslourenco@gmail.com>
2024-07-29 17:20:45 -0300

    feat: add api gateway

---
+++ api_gateway/src/main/java/com/e_commerce/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.e_commerce.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Value("${services.users.uri}")
+    private String USERS_SERVICE;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("users_service", r -> r.path("/users/**", "/auth/**", "/swagger-ui/**", "/common/**", "/restricted/**")
+                        .uri(USERS_SERVICE))
+                .build();
+    }
+
+}
